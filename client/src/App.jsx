fd1ee7dfc3ea214549c206106a144932022e60e8
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-05-31 07:19:44 +0530

    Create App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
