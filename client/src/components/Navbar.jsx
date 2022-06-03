a550bbf541c8d8e8018c28d784dd5084a4484097
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-03 09:05:45 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     return (
         <nav className={"w-full flex md:justify-center justify-between items-center p-4"}>
             <div className={"md:flex-[0.5] flex-initial justify-center items-center"}>
-                <img src={logo} alt="cryptoverseWeb3 logo" className={"w-32 cursor-pointer"}/>
+                <img src={logo} alt="cryptoverseWeb3 logo" className={"w-48 cursor-pointer"}/>
             </div>
 
             <ul className={"text-white md:flex hidden list-none flex-row justify-between items-center flex-initial"}>
