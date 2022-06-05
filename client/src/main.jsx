b02942a96400bfa062537faa5838f1c5dad5708b
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-05 20:25:29 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import {TransactionProvider} from "./context/TransactionContext";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <TransactionProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+    </TransactionProvider>
 )
