e33a9c8a3c5c1c8b2f574ffed1ab060b10129de5
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-06 21:16:58 +0530

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,9 +5,8 @@ import {SiEthereum} from "react-icons/si";
 import {BsInfoCircle} from "react-icons/bs";
 
 import {TransactionContext} from "../context/TransactionContext";
-
-
 import {Loader} from './'
+
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
 
@@ -23,16 +22,21 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 );
 
 const Welcome = () => {
-    const {connectWallet, currentAccount} = useContext(TransactionContext);
+    const {connectWallet, currentAccount, formData, sendTransaction, handleChange} = useContext(TransactionContext);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        const {addressTo, amount, keyword, message} = formData;
+        e.preventDefault()
+
+        if(!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
     }
 
     return (
         <div className={"flex w-full justify-center items-center"}>
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
                 <dev className="flex flex-1 justify-start flex-col mf:mr-10">
-
                     <h1 className={"text-3xl sm:text-5xl text-white text-gradient py-1"}>
                         Send Crypto <br/> across the world
                     </h1>
@@ -40,6 +44,7 @@ const Welcome = () => {
                     <p className={"text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base"}>
                         Explore the crypto world. Buy and sell cryptocurrencies easily on CryptoverseWeb3.
                     </p>
+
                     {!currentAccount && (
                         <button
                             type={"button"}
@@ -47,7 +52,8 @@ const Welcome = () => {
                             className={"flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"}
                         >
                             <p className={"text-white text-base font-semibold "}>Connect Wallet</p>
-                        </button>)}
+                        </button>
+                    )}
 
                     <div className={"grid sm:grid-cols-3 grid-cols-2 w-full mt-10"}>
                         <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
@@ -83,14 +89,10 @@ const Welcome = () => {
                     </div>
 
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder={"Address To"} name={"addressTo"} type={"text"} handleChange={() => {
-                        }}/>
-                        <Input placeholder={"Amount (ETH)"} name={"amount"} type={"number"} handleChange={() => {
-                        }}/>
-                        <Input placeholder={"Keyword (Gif)"} name={"keyword"} type={"text"} handleChange={() => {
-                        }}/>
-                        <Input placeholder={"Enter Message"} name={"message"} type={"text"} handleChange={() => {
-                        }}/>
+                        <Input placeholder={"Address To"} name={"addressTo"} type={"text"} handleChange={handleChange}/>
+                        <Input placeholder={"Amount (ETH)"} name={"amount"} type={"number"} handleChange={handleChange}/>
+                        <Input placeholder={"Keyword (Gif)"} name={"keyword"} type={"text"} handleChange={handleChange}/>
+                        <Input placeholder={"Enter Message"} name={"message"} type={"text"} handleChange={handleChange}/>
 
                         <div className="h-[1px] w-full bg-gray-400 my-2 eth-card"/>
 
@@ -107,9 +109,7 @@ const Welcome = () => {
                         )}
                     </div>
 
-
                 </div>
-
             </div>
         </div>
     );
