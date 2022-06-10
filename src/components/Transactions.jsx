b38fe5010bee29beb4ee9a9a7c6198aaa324cdb0
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-10 12:55:17 +0530

    Update Transactions.jsx

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,13 +1,12 @@
 import React, {useContext} from 'react';
 
 import {TransactionContext} from "../context/TransactionContext";
-import dummyData from "../utils/dummyData";
+// import dummyData from "../utils/dummyData";
 import useFetch from "../hooks/useFetch";
 import {shortenAddress} from "../utils/shortenAddress";
 
 
-
-const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
+const TransactionsCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
     const gifUrl = useFetch({keyword});
     return (
         <div className={"bg-[#181918] m-4 flex flex-1" +
@@ -19,19 +18,19 @@ const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, a
         }>
             <div className={"flex flex-col items-center w-full mt-3"}>
                 <div className={"display-flex w-full mb-6 p-2"}>
-                    <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target={"_blank"} rel={"noopener noreferrer"}>
+                    <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target={"_blank"}
+                       rel={"noopener noreferrer"}>
                         <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
                     </a>
-                    <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target={"_blank"} rel={"noopener noreferrer"}>
+                    <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target={"_blank"}
+                       rel={"noopener noreferrer"}>
                         <p className="text-white text-base">From: {shortenAddress(addressTo)}</p>
                     </a>
                     <p className={"text-white text-base"}>Amount: {amount} ETH</p>
-                    {message && (
-                        <>
+                    {message && (<>
                         <br/>
-                            <p className={"text-white text-base"}>Message: {message}</p>
-                        </>
-                    )}
+                        <p className={"text-white text-base"}>Message: {message}</p>
+                    </>)}
                 </div>
 
                 <img
@@ -39,38 +38,40 @@ const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, a
                     alt={"gif"}
                     className={"w-full h-64 2x:h96 rounded-md shadow-lg object-cover"}
                 />
-                
+
                 <div className={"bg-black p-3 px-5 w-max rounded-2xl -mt-5 shadow-2xl"}>
                     <p className={"text-[#37a7da] font-bold"}>{timestamp}</p>
                 </div>
             </div>
         </div>
-
     );
 }
 
 const Transactions = () => {
-    const {currentAccount} = useContext(TransactionContext);
+    const {currentAccount, transactions} = useContext(TransactionContext);
 
     return (
         <div className={"flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions"}>
-            <h1>
-                Transactions
-            </h1>
             <div className={"flex flex-col md:p-12 py-12 px-4"}>
                 {currentAccount ? (
-                    <h3 className={"text-white text-3xl text-center my-2"}>Latest transactions</h3>
+                    <h3 className={"text-white text-3xl text-center my-2"}>
+                        Latest transactions
+                    </h3>
                 ) : (
-                    <h3 className={"text-white text-3xl text-center my-2"}>Connect your account to see the latest transactions</h3>
+                    <h3 className={"text-white text-3xl text-center my-2"}>
+                        Connect your account to see the latest transactions
+                    </h3>
                 )}
 
                 <div className={"flex flex-wrap justify-center items-center mt-10"}>
-                    {[dummyData.reverse().map((transaction, i) => (
-                        <TransactionCard key={i} {...transaction}/>
-                    ))]}
+                    {[
+                        // ...dummyData,
+                        ...transactions
+                    ].reverse().map((transaction, i) => (
+                        <TransactionsCard key={i} {...transaction}/>
+                    ))}
                 </div>
             </div>
-
         </div>
 
     );
