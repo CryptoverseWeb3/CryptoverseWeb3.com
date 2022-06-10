a6ce5fcc0788e526d8e2e52c85281ef0608397b7
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-10 12:55:20 +0530

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -23,7 +23,7 @@ const Input = ({placeholder, name, type, value, handleChange}) => (
 );
 
 const Welcome = () => {
-    const {connectWallet, currentAccount, formData, sendTransaction, handleChange} = useContext(TransactionContext);
+    const {connectWallet, currentAccount, formData, sendTransaction, handleChange, isLoading} = useContext(TransactionContext);
 
     const handleSubmit = (e) => {
         const {addressTo, amount, keyword, message} = formData;
@@ -97,7 +97,7 @@ const Welcome = () => {
 
                         <div className="h-[1px] w-full bg-gray-400 my-2 eth-card"/>
 
-                        {false ? (
+                        {isLoading ? (
                             <Loader/>
                         ) : (
                             <button
