411e4c40f24a038465ad195ec670f520e26d4bdb
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-11 19:54:23 +0530

    Update main.jsx
    Co-Authored-By: Kabir  <44284877+kabir0x23@users.noreply.github.com>

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import {TransactionsProvider} from "./context/TransactionContext";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <TransactionsProvider>
       <React.StrictMode>
-        <Router basename={"/"}>
+        <Router basename={"/cryptoverseWeb3"}>
           <Provider store={store}>
             <App/>
           </Provider>
