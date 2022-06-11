3ce93e3f2a56a1aa1a7dc98a81e8d1f045b46b13
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-11 15:24:27 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,10 @@
 export {default as Loader} from './Loader'
 export {default as Navbar} from './Navbar'
+export {default as Homepage} from './Homepage'
 export {default as Welcome} from './Welcome'
 export {default as Services} from './Services'
 export {default as Transactions} from './Transactions'
+
+export {default as Cryptocurrencies} from './cryptocurrencies/Cryptocurrencies';
+
 export {default as Footer} from './Footer'
