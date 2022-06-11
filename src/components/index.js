09051330361e47e4f349d3aa01d06eb7e5c29fec
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-11 15:59:42 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export {default as Loader} from './Loader'
-export {default as Homepage} from './homepage';
+export {default as Homepage} from './Homepage';
 export {default as Navbar} from './Navbar'
 export {default as Welcome} from './Welcome'
 export {default as Services} from './Services'
