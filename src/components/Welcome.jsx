a1bbe06b4b18f203bcb50dad8e10f62806018629
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-11 19:53:48 +0530

    Update Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,13 @@
 import React, {useContext} from 'react';
 
-// import {AiFillPlayCircle} from "react-icons/ai";
+import {AiFillPlayCircle} from "react-icons/ai";
 import {SiEthereum} from "react-icons/si";
 import {BsInfoCircle} from "react-icons/bs";
 
 import {TransactionContext} from "../context/TransactionContext";
 import {shortenAddress} from "../utils/shortenAddress";
 import {Loader} from './'
+// import {AiFillPlayCircle} from "react-icons/all";
 
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
@@ -46,13 +47,16 @@ const Welcome = () => {
                         Explore the crypto world. Buy and sell cryptocurrencies easily on CryptoverseWeb3.
                     </p>
 
-          {!currentAccount && (
+                    {!currentAccount && (
                         <button
-                            type={"button"}
+                            type="button"
                             onClick={connectWallet}
-                            className={"flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"}
+                            className="flex flex-row justify-center items-center my-5 bg-[#F58610]  rounded-lg cursor-pointer hover:bg-[#D67610]"
                         >
-                            <p className={"text-white text-base font-semibold "}>Connect Wallet</p>
+                            <AiFillPlayCircle className="text-white mr-2" />
+                            <p className="text-white text-base font-semibold mt-3">
+                                Connect Wallet
+                            </p>
                         </button>
                     )}
 
@@ -72,42 +76,45 @@ const Welcome = () => {
                 </div>
 
                 <div className={"flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10"}>
-                    <div
-                        className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full eth-card white-glassmorphism ">
+                    <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
-                                <div
-                                    className={"w-10 h-10 rounded-full border-2 border-white flex justify-center items-center"}>
-                                    <SiEthereum fontSize={21} color={"#fff"}/></div>
+                                <div className={"w-10 h-10 rounded-full border-2 border-white flex justify-center items-center"}>
+                                    <SiEthereum fontSize={21} color={"#fff"}/>
+                                </div>
                                 <BsInfoCircle fontSize={17} color={"#fff"}/>
                             </div>
 
                             <div>
-                                <p className={"text-white font-light text-sm"}>Address: <br/> {shortenAddress(currentAccount)}</p>
-                                <p className={"text-white font-semibold text-lg mt-1"}>Ethereum</p>
+                                <p className={"text-white font-light text-sm"}>
+                                    Address: <br/> {shortenAddress(currentAccount)}
+                                </p>
+                                <p className={"text-white font-semibold text-lg mt-1"}>
+                                    Ethereum
+                                </p>
                             </div>
                         </div>
                     </div>
 
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder={"Address To"} name={"addressTo"} type={"text"} handleChange={handleChange}/>
-                        <Input placeholder={"Amount (ETH)"} name={"amount"} type={"number"} handleChange={handleChange}/>
-                        <Input placeholder={"Keyword (GIF)"} name={"keyword"} type={"text"} handleChange={handleChange}/>
-                        <Input placeholder={"Enter Message"} name={"message"} type={"text"} handleChange={handleChange}/>
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
-                        <div className="h-[1px] w-full bg-gray-400 my-2 eth-card"/>
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-                        {isLoading ? (
-                            <Loader/>
-                        ) : (
-                            <button
-                                type={"button"}
-                                onClick={handleSubmit}
-                                className={"text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"}
-                            >
-                                Send Now
-                            </button>
-                        )}
+                        {isLoading
+                            ? <Loader />
+                            : (
+                                <button
+                                    type="button"
+                                    onClick={handleSubmit}
+                                    className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                                >
+                                    Send now
+                                </button>
+                            )}
                     </div>
 
                 </div>
