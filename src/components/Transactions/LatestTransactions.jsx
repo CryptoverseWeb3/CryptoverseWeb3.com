03523b817bbfeeaa012837c2b99a71cc11d24b40
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-12 16:55:22 +0530

    transactions-effect
    white-glassmorphism effect on transactions

--- src/components/Transactions/LatestTransactions.jsx
+++ src/components/Transactions/LatestTransactions.jsx
@@ -10,13 +10,19 @@ import {Link} from "react-router-dom";
 const TransactionsCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
     const gifUrl = useFetch({keyword});
     return (
-        <div className={"bg-[#181918] m-4 flex flex-1" +
+        <div className={"white-glassmorphism  m-4 flex flex-1" +
             "2xl:min-w-[450px]" +
             "2xl:max-w-[500px]" +
             "sm:min-w-[270px]" +
             "sm:max-w-[300px]" +
             "flex-col p-3 rounded-md hover:shadow-2xl"
         }>
+            {/*<div className={`w-10 h-10 rounded-full flex justify-center items-center`}>*/}
+            {/*    <div className={"flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl"}>*/}
+            {/*        /!*{icon}*!/*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+
             <div className={"flex flex-col items-center w-full mt-3"}>
                 <div className={"display-flex w-full mb-6 p-2"}>
                     <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target={"_blank"}
@@ -41,7 +47,7 @@ const TransactionsCard = ({addressTo, addressFrom, timestamp, message, keyword, 
                     className={"w-full h-64 2x:h96 rounded-md shadow-lg object-cover"}
                 />
 
-                <div className={"bg-black p-3 px-5 w-max rounded-2xl -mt-5 shadow-2xl"}>
+                <div className={"bg-black white-glassmorphism  p-3 px-5 w-max rounded-2xl -mt-5 shadow-2xl"}>
                     <p className={"text-[#37a7da] font-bold"}>{timestamp}</p>
                 </div>
             </div>
--- src/components/Transactions/TransactionsHistory.jsx
+++ src/components/Transactions/TransactionsHistory.jsx
@@ -10,7 +10,7 @@ import {AiFillPlayCircle} from "react-icons/ai";
 const TransactionsCard = ({addressTo, addressFrom, timestamp, message, keyword, amount, url}) => {
     const gifUrl = useFetch({keyword});
     return (
-        <div className={"bg-[#181918] m-4 flex flex-1" +
+        <div className={"white-glassmorphism m-4 flex flex-1" +
             "2xl:min-w-[450px]" +
             "2xl:max-w-[500px]" +
             "sm:min-w-[270px]" +
