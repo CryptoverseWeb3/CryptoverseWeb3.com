7fc078f756542cbfbf4561573ef0e109004193bf
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-12 13:02:38 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,9 @@ export {default as Homepage} from './Homepage';
 export {default as Navbar} from './Navbar';
 export {default as Welcome} from './Welcome';
 export {default as Services} from './Services';
-
 export {default as Transactions} from './Transactions/Transactions';
 export {default as TransactionsHistory} from './Transactions/TransactionsHistory';
-
-export {default as Cryptocurrencies} from './cryptocurrencies/Cryptocurrencies';
-export {default as CryptoDetails} from './cryptocurrencies/CryptoDetails';
-export {default as News} from './cryptocurrencies/News';
-
+export {default as Cryptocurrencies} from './Cryptocurrencies/Cryptocurrencies';
+export {default as CryptoDetails} from './Cryptocurrencies/CryptoDetails';
+export {default as News} from './Cryptocurrencies/News';
 export {default as Footer} from './Footer';
