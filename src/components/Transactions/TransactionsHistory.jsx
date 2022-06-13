54a4757ea8527418abd75ac285a5efd392d4a654
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-13 21:35:01 +0530

    Update TransactionsHistory.jsx

--- src/components/Transactions/TransactionsHistory.jsx
+++ src/components/Transactions/TransactionsHistory.jsx
@@ -55,23 +55,13 @@ const Transactions = () => {
         <div className={"flex w-full justify-center items-center 2xl:px-20"}>
             <div className={"flex flex-col md:p-12 py-12 px-4"}>
 
-                {!currentAccount ? (
-                        <h3 className={"text-gray-300 text-3xl text-center my-2 "}>
-                            <a onClick={connectWallet} className={"text-amber-500"}>
-                                Connect your account <br/> </a>
-                            to see the latest transactions
-                            <img
-                                className={"eth-logo w-full  h-96 2x:h96 rounded-md shadow-lg object-cover mt-5 "}
-                                src={"https://media3.giphy.com/media/Hx13ouuEzEff1GbcpJ/giphy.gif?cid=790b76116e9aeb1aa06c0af621e73ac96ef317de52d8fdea&rid=giphy.gif&ct=g"}
-                                alt={"transactions-spinner"}
-                            />
-                        </h3>
-                ) : (
+                {currentAccount ? (
                     transactions.length > 0 && (
                         <h3 className={"text-gradient text-white text-4xl text-center my-2 pt-5"}>
                             10 Latest Transactions
                         </h3>
                     )
+
                     || transactions.length === 0 && (
                         <h3 className={"text-gray-300 text-3xl text-center my-2 "}>
                             <p onClick={connectWallet} className={"text-gray-300"}>
@@ -84,6 +74,18 @@ const Transactions = () => {
                             </p>
                         </h3>
                     )
+
+                ) : (
+                    <h3 className={"text-gray-300 text-3xl text-center my-2 "}>
+                        <a onClick={connectWallet} className={"text-amber-500"}>
+                            Connect your account <br/> </a>
+                        to see the latest transactions
+                        <img
+                            className={"eth-logo w-full  h-96 2x:h96 rounded-md shadow-lg object-cover mt-5 "}
+                            src={"https://media3.giphy.com/media/Hx13ouuEzEff1GbcpJ/giphy.gif?cid=790b76116e9aeb1aa06c0af621e73ac96ef317de52d8fdea&rid=giphy.gif&ct=g"}
+                            alt={"transactions-spinner"}
+                        />
+                    </h3>
                 )}
 
                 <div className={"flex flex-wrap justify-center items-center mt-10"}>
@@ -92,8 +94,7 @@ const Transactions = () => {
                         ...transactions
                     ].reverse().map((transaction, i) => (
                         <TransactionsCard
-                            key={i}
-                            {...transaction}
+                            key={i} {...transaction}
                         />
                     ))}
                 </div>
