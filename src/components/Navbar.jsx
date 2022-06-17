5b279ef4d2175a9a6d6a181df58a3bb5cb523eb6
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-06-17 17:42:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,10 @@
-import React
-// ,{ useState}
-    from 'react';
+import React from 'react';
 
-// import {HiMenuAlt4} from 'react-icons/hi'
-// import {AiOutlineClose} from 'react-icons/ai'
 import {RiMenuFoldLine} from 'react-icons/ri'
 import {CgCloseR} from 'react-icons/cg'
 import {Link} from 'react-router-dom';
 
 import logo from '../../images/cryptoverseWeb3.png'
-import {TransactionsHistory} from "./index";
 
 const NavbarItem = ({title, classProps}) => {
     return (
@@ -18,6 +13,7 @@ const NavbarItem = ({title, classProps}) => {
         </li>
     )
 }
+
 
 const Navbar = () => {
     const [toggleMenu, setToggleMenu] = React.useState(false);
@@ -31,7 +27,6 @@ const Navbar = () => {
 
             <ul className={"navbar-size text-white md:flex hidden list-none flex-row justify-between items-center flex-initial"}>
                 {[
-                    // <Link className={"text-white"} to='/'>Home</Link>,
                     <Link className={"text-white"} to='/market'>Market</Link>,
                     <Link className={"text-white"} to='/cryptocurrencies'>Cryptocurrencies</Link>,
                     <Link className={"text-white"} to='/news'>Crypto News</Link>,
@@ -43,28 +38,23 @@ const Navbar = () => {
                 ].map((item, index) => (
                     <NavbarItem key={item + index} title={item}/>
                 ))}
-
-                {/*<li className={"bg-[] py-2 px-5 mx-4 rounded-lg cursor-pointer hover:bg-[#ed830e]"}>*/}
-                {/*    <a className={"text-white"} href="https://www.linktree.com/CryptoverseWeb3" target={"_blank"}> Social links*/}
-                {/*    </a>*/}
-                {/*</li>*/}
             </ul>
 
             <div className={"flex relative"}>
                 {toggleMenu
                     ? <CgCloseR fontsize={28} className={"hidden text-white md:hidden sm:hidden cursor-pointer"}
-                                      onClick={() => setToggleMenu(false)}/>
+                                onClick={() => setToggleMenu(false)}/>
                     : <RiMenuFoldLine fontsize={28} className={"text-3xl text-white md:hidden cursor-pointer"}
-                                  onClick={() => setToggleMenu(true)}/>
+                                      onClick={() => setToggleMenu(true)}/>
                 }
+
                 {toggleMenu && (
                     <ul className={"z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2x1 md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"}>
                         <li className={"text-xl w-full my-2"}>
                             <CgCloseR className={"bg-gray-800 m-5 text-3xl"}
-                                            onClick={() => setToggleMenu(false)}/>
+                                      onClick={() => setToggleMenu(false)}/>
                         </li>
                         {[
-
                             <Link className={"text-white"} to='/'>Home</Link>,
                             <Link className={"text-white"} to='/market'>Market</Link>,
                             <Link className={"text-white"} to='/cryptocurrencies'>Cryptocurrencies</Link>,
@@ -77,12 +67,14 @@ const Navbar = () => {
                         ].map((item, index) => (
                             <NavbarItem key={item + index} title={item}/>
                         ))}
-                    <div className={"mt-5"}>
-                        <li className={"bg-[#ed830e] py-1 px-4 mx-1 rounded-lg cursor-pointer hover:bg-[#f26c3c]"}>
-                            <a className={"text-white"} href="https://www.linktree.com/CryptoverseWeb3" target={"_blank"}> Social links
-                            </a>
-                        </li>
-                    </div>
+
+                        <div className={"mt-5"}>
+                            <li className={"bg-[#ed830e] py-1 px-4 mx-1 rounded-lg cursor-pointer hover:bg-[#f26c3c]"}>
+                                <a className={"text-white"} href="https://www.linktree.com/CryptoverseWeb3"
+                                   target={"_blank"}> Social links
+                                </a>
+                            </li>
+                        </div>
                     </ul>
                 )}
 
