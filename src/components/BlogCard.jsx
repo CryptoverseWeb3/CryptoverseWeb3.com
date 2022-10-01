b02b1190d5817680840154b1256ce0c023c3c4f6
Manthan Kuber <manthankuber873@gmail.com>
2022-10-01 10:14:30 +0530

    add blog list

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -4,9 +4,9 @@ import {
   CommentOutlined,
 } from "@ant-design/icons";
 
-const BlogCard = ({ category, title, viewCount, commentCount }) => {
+const BlogCard = ({ category, title, content, viewCount, commentCount }) => {
   return (
-    <article className="max-w-sm overflow-hidden transition-shadow duration-150 ease-in-out border border-gray-200 rounded-lg shadow-sm cursor-pointer hover:shadow-lg">
+    <article className="max-w-md overflow-hidden transition-shadow duration-150 ease-in-out border border-gray-200 rounded-lg shadow-sm cursor-pointer hover:shadow-lg">
       <img
         className="w-full"
         src="https://picsum.photos/id/237/720/400"
@@ -16,8 +16,8 @@ const BlogCard = ({ category, title, viewCount, commentCount }) => {
         <h5 className="mb-1 text-sm font-semibold text-gray-500 uppercase">
           {category}
         </h5>
-        <h3 className="mb-3 text-2xl font-semibold">Title</h3>
-        <p className="text-base text-gray-700">{title}</p>
+        <h3 className="mb-3 text-2xl font-semibold">{title}</h3>
+        <p className="text-base text-gray-700 ">{content}</p> 
       </div>
       <div className="flex justify-between px-6 pt-4 pb-2">
         <div className="flex items-center gap-1 font-semibold text-blue-500 group ">
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,9 +1,22 @@
 import BlogCard from "./BlogCard";
 
+const blogList = [...Array(12).keys()].map((_, key) => {
+  return (
+    <BlogCard
+      key={key}
+      category="Pets"
+      title="Adopt Cute Pets"
+      content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla cursus enim magna, at suscipit ex mattis id. Pellentesque efficitur nunc nibh, ac cursus erat placerat nec. Donec tincidunt quam iaculis, porta turpis non, sodales eros. Phasellus aliquam tempus lectus sed hendrerit."
+      commentCount={20}
+      viewCount={6}
+    />
+  );
+});
+
 const Blogs = () => {
   return (
-    <div>
-      <BlogCard />
+    <div className="grid gap-y-6 sm:gap-6 2xl:gap-10  xl:gap-y-8 mx-auto justify-items-center sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+      {blogList}
     </div>
   );
 };
