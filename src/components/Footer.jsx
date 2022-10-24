8cb89f3b8e5f0fbb286e3cfbc737e589718247b0
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2022-10-24 12:11:34 +0530

    add: other card transparent as same as crypto cards [Fixes #25] (#29)
    * Add Social icons to the footer with hover effects
    
    * Making other card transparent as same as crypto cards
    
    * Update CHANGELOG.md
    
    Co-authored-by: Kabir <44284877+kabir0x23@users.noreply.github.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,13 @@ const Footer = () => {
             <div className="sm:w-[90%] w-full h-[1.25px] eth-card m-5 "/>
             <div className={"sm:w-[90%] w-full flex justify-between items-center"}>
                 <img src={logo} alt="CryptoverseWeb3 logo" className={"w-48"}/>
+                
+                <div class="container-fluid">
+                <i class="social-icon fa-brands fa-twitter"></i>
+                <i class="social-icon fa-brands fa-facebook-f"></i>
+                <i class="social-icon fa-brands fa-instagram"></i>
+                <i class="social-icon fa-solid fa-envelope"></i>
+                </div>
                 <p className={"text-white text-sm text-center mr-20"}>@CryptoverseWeb3 2022</p>
                 <p className={"text-white text-sm text-center"}>All rights reserved</p>
             </div>
